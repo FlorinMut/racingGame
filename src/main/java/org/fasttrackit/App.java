9c53b426b70bfff7eab04ee69827d7941fadce93
Flo <florinmut93@gmail.com>
2018-12-13 21:31:17 +0200

    create car obiect si clasa

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -2,12 +2,39 @@ package org.fasttrackit;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Car car = new Car(); /**instantierea unui obiect, crearea unui obiect din clasa, car e variabila, imi creaaza variabila car, ca sa mai faco variabila, din nou car car2, imi va creaza a 2 a maasina*/
+        car.name = "Dacia"; /** cu alt selectam ce vrem pe verticala*/
+        car.color ="white";
+        car.doorCount = 4;
+        car.mileage = 7.5;
+        car.running = true;
+
+        /** metoda 1 pt a adauga si motor */
+
+        Engine engine = new Engine();
+        engine.manufacturer = "Renault";
+        engine.capacity = 1500;
+
+        car.engine = engine; /** motorul care l-am creat il atribuim masinii noastre. Daca nu faecm asta, motorul exista, dar nu e folosit*/
+
+        /** metoda 2 pt a adauga si motor */
+
+        car.engine = new Engine(); /**e o relatie de compozitie, clasa car contine un obiect de tip as a, contine un obiect engine */
+
+        engine = null; /** mai avem motorul, nu se sterge, dar nu il mai folosm */
+
+
+
+
+
+
     }
+
+
 }
+
+
+
---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,18 @@
+package org.fasttrackit;
+
+public class Car {
+
+    String name;
+    String color;
+    int doorCount;
+    double mileage;
+    boolean running = true;
+    Engine engine; /** poate sa ii zica altfel */
+
+
+
+
+
+
+
+}
