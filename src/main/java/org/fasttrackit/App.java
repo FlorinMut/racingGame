4ca4bf830ecf32c728b6d7c89292585622767898
Flo <florinmut93@gmail.com>
2018-12-20 21:26:49 +0200

    Studied incapsulation

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -7,21 +7,26 @@ import java.time.LocalDateTime;
  */
 public class App {
     public static void main(String[] args) {
-        Car car = new Car(); /**instantierea unui obiect, crearea unui obiect din clasa, car e variabila, imi creaaza variabila car, ca sa mai faco variabila, din nou car car2, imi va creaza a 2 a maasina*/
-        car.name = "Dacia"; /** cu alt selectam ce vrem pe verticala*/
-        car.color ="white";
-        car.doorCount = 4;
-        car.mileage = 7.5;
-        car.running = true;
-
-        double carTraveledDistance = car.accelerate(100,2);
-
-        /** metoda 1 pt a adauga si motor */
 
         Engine engine = new Engine();
         engine.manufacturer = "Renault";
         engine.capacity = 1500;
         engine.expiryDate = LocalDateTime.now().plusYears(10);
+
+        Car car = new Car(engine);/**instantierea unui obiect, crearea unui obiect din clasa, car e variabila, imi creaaza variabila car, ca sa mai faco variabila, din nou car car2, imi va creaza a 2 a maasina*/
+        car.setName ("Dacia");
+        car.setColor ("white");
+        car.doorCount = 4;
+        car.setMileage (7.5);
+        car.running = true;
+
+        System.out.println(car.getName());
+
+        double carTraveledDistance = car.accelerate(100,2);
+
+        /** metoda 1 pt a adauga si motor */
+
+
 
         car.engine = engine; /** motorul care l-am creat il atribuim masinii noastre. Daca nu faecm asta, motorul exista, dar nu e folosit*/
 
--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -2,13 +2,37 @@ package org.fasttrackit;
 
 public class Vehicle {
 
-    String name;
-    String color;
-    double mileage;
+    private String name; //tin alt si scriu pe toate liniile deodata, am facut toate propr private -> eroare la vehicle fiindca nu le mai vede acolo, e private/
+    private String color;
+    private double mileage;
 
     // public void - nu returneaza nimic
 
-    public double accelerate(double speed, double durationInHours){ //semnatura metodei cum se numeste si ce returneaza, parametrii sunt tip de data si denumire/
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name; //this.name = name.trim() - pt incapsulare, trim va sterge spatii de peste tot unde apare numele/
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
+    }
+
+    protected double accelerate(double speed, double durationInHours){ //semnatura metodei cum se numeste si ce returneaza, parametrii sunt tip de data si denumire/
 
         System.out.println(name + "acceletaed with" + speed + "km/h" + durationInHours + "hours.");
 
@@ -17,6 +41,8 @@ public class Vehicle {
         System.out.println("Traveled distance:" + traveledDistance);
 
         return traveledDistance;
+
+
 
 
     }
